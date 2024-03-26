15236986ecbc9dc2aa813c532af6746eb5ba9dd5
Jun1oru <andreicobra35v@gmail.com>
2024-03-26 20:07:36 +0200

    Package fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import createMenu from "./menu";
 import createMenuElementDom from "./menu-dom";
 import "./styles.css";
 
-const body = document.querySelector("body");
+/* const body = document.querySelector("body");
 createMenu("Test", "testMenu", body);
 createMenuElementDom("Home", "homeElement", "testMenu", "https://google.com");
 createMenuElementDom(
@@ -10,4 +10,4 @@ createMenuElementDom(
   "aboutElement",
   "testMenu",
   "https://youtube.com",
-);
+); */
